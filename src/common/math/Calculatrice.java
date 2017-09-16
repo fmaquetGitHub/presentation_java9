40a2cabed02b02ae4a153dbd0008d80434fa8d14
Fred M <frederic.maquet@gmail.com>
2017-09-16 15:04:28 +0200

    [JAVA8] : Hello World !

---
+++ src/common/math/Calculatrice.java
@@ -0,0 +1,8 @@
+package common.math;
+
+public class Calculatrice{
+
+	public int somme(int a, int b){
+		return a+b;
+	}
+}
---
+++ src/norsys/main/Main.java
@@ -0,0 +1,12 @@
+package norsys.main;
+
+import common.math.*;
+
+public class Main{
+
+	public static void main(String args[]){
+		String helloWorld = new String("Hello World ! ");
+		Calculatrice calculatrice = new Calculatrice();
+		System.out.println(helloWorld + calculatrice.somme(1, 1));
+	}
+}
