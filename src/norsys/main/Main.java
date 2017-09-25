8c3d6559bd99ce652a04471f3546f488fd985a8d
FREDERIC MAQUET <frederic.maquet@ext.adeo.com>
2017-09-25 17:31:07 +0200

    [JAVA 9] : Premier modules

--- src/norsys/main/Main.java
+++ src/norsys/main/Main.java
@@ -1,12 +1,10 @@
 package norsys.main;
 
-import common.math.*;
+public class Main {
 
-public class Main{
+ public static void main(String args[]) {
+     String helloWorld = new String("Hello World ! ");
+     System.out.println(helloWorld);
+ }
 
-	public static void main(String args[]){
-		String helloWorld = new String("Hello World ! ");
-		Calculatrice calculatrice = new Calculatrice();
-		System.out.println(helloWorld + calculatrice.somme(1, 1));
-	}
 }
